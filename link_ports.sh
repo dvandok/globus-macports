87e9df33a5fa1c5f6dc8ecc9e811507a66856ca8
Dennis van Dok <dennisvd@nikhef.nl>
2013-03-05 15:30:31 +0100

    - Use /bin/sh - remove useless 'function' keyword - turn the source is an absolute path if it isn't - remove a few unneeded commands such as 'ls' and 'grep'

--- link_ports.sh
+++ link_ports.sh
@@ -1,9 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
+# This script creates a macports directory structure of portfiles
+# based on their category. The first argument is the directory that
+# holds our entire collection of ports, the second argument is the
+# target port directory. The directory structure will be populated
+# with symbolic links to their original.
+#
+# This script needs to be run whenever new ports are added to/removed
+# from our collection.
+#
+# If portfiles are otherwise updated, it is sufficient to run the
+# portindex utility in the target directory.
+#
+# Bugs: this script can't handle multiple categories per Portfile
 
 SOURCE="$1"
 PORTS_DIR="$2"
 
-function usage()
+usage()
 {
     echo "Usage:"
     echo "  link_ports <source directory> <local ports repository>"
@@ -16,21 +30,24 @@ if [ -z "$SOURCE" -o -z "$PORTS_DIR" ]; then
     exit -1
 fi
 
+# Make source dir absolute; otherwise the symbolic links will point nowhere
+if ! `echo "$SOURCE" | grep -q '^/'` ; then
+    SOURCE=`pwd`/"$SOURCE"
+fi
+
 # Clean ports dir
 if [ -d "$PORTS_DIR" ]; then
     rm -rf "$PORTS_DIR"
 fi
 
-PORTS=`ls "$SOURCE"`
-
 mkdir -p $PORTS_DIR
 
-for port in $PORTS; do
+for port in "$SOURCE"/* ; do
     if [ ! -f "$SOURCE/$port/Portfile" ]; then
         continue
     fi
 
-    category=`grep "^categories" "$SOURCE/$port/Portfile" | awk '{print $2}'`
+    category=`awk '/^categories/ {print $2}'  "$SOURCE/$port/Portfile"`
 
     if [ "$?" != "0" ]; then
         echo "error"
